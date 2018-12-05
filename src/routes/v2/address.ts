305c967a37ec886b73aee6ba6a958fa5ccb13026
Chris Troutner <chris.troutner@gmail.com>
2018-12-05 06:26:12 -0800

    Refactored address.js to address.ts

--- src/routes/v2/address.ts
+++ src/routes/v2/address.ts
@@ -1,11 +1,14 @@
 "use strict"
 
-const express = require("express")
-const router = express.Router()
-const axios = require("axios")
-const RateLimit = require("express-rate-limit")
+import * as express from "express"
+import * as requestUtils from "./services/requestUtils"
+import axios from "axios"
 const logger = require("./logging.js")
 const routeUtils = require("./route-utils")
+
+//const router = express.Router()
+const router: express.Router = express.Router()
+const RateLimit = require("express-rate-limit")
 
 // Used for processing error messages before sending them to the user.
 const util = require("util")
@@ -14,11 +17,20 @@ util.inspect.defaultOptions = { depth: 1 }
 const BITBOXCli = require("bitbox-sdk/lib/bitbox-sdk").default
 const BITBOX = new BITBOXCli()
 
-const config = {
+interface IRLConfig {
+  [addressRateLimit1: string]: any
+  addressRateLimit2: any
+  addressRateLimit3: any
+  addressRateLimit4: any
+  addressRateLimit5: any
+}
+
+const config: IRLConfig = {
   addressRateLimit1: undefined,
   addressRateLimit2: undefined,
   addressRateLimit3: undefined,
-  addressRateLimit4: undefined
+  addressRateLimit4: undefined,
+  addressRateLimit5: undefined
 }
 
 let i = 1
@@ -27,7 +39,7 @@ while (i < 6) {
     windowMs: 60000, // 1 hour window
     delayMs: 0, // disable delaying - full speed until the max limit is reached
     max: 60, // start blocking after 60 requests
-    handler: function(req, res /*next*/) {
+    handler: function(req: express.Request, res: express.Response /*next*/) {
       res.format({
         json: function() {
           res.status(500).json({
@@ -48,14 +60,22 @@ router.post("/unconfirmed/:address", config.addressRateLimit4, unconfirmed)
 router.post("/transactions/:address", config.addressRateLimit5, transactions)
 
 // Root API endpoint. Simply acknowledges that it exists.
-function root(req, res, next) {
+function root(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
   return res.json({ status: "address" })
 }
 
 // Retrieve details on an address.
 // curl -d '{"addresses": ["bchtest:qzjtnzcvzxx7s0na88yrg3zl28wwvfp97538sgrrmr", "bchtest:qp6hgvevf4gzz6l7pgcte3gaaud9km0l459fa23dul"]}' -H "Content-Type: application/json" http://localhost:3000/v2/address/details
 // curl -d '{"addresses": ["bchtest:qzjtnzcvzxx7s0na88yrg3zl28wwvfp97538sgrrmr", "bchtest:qp6hgvevf4gzz6l7pgcte3gaaud9km0l459fa23dul"], "from": 1, "to": 5}' -H "Content-Type: application/json" http://localhost:3000/v2/address/details
-async function details(req, res, next) {
+async function details(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
   try {
     const addresses = req.body.addresses
 
@@ -112,20 +132,28 @@ async function details(req, res, next) {
     // Return the array of retrieved address information.
     res.status(200)
     return res.json(retArray)
-  } catch (error) {
-    // Write out error to error log.
-    //logger.error(`Error in address/details: `, error)
+  } catch (err) {
+    // Attempt to decode the error message.
+    const { msg, status } = routeUtils.decodeError(err)
+    if (msg) {
+      res.status(status)
+      return res.json({ error: msg })
+    }
 
-    // Return error message to the caller.
+    // Write out error to error log.
+    //logger.error(`Error in rawtransactions/decodeRawTransaction: `, err)
+
     res.status(500)
-    if (error.response && error.response.data && error.response.data.error)
-      return res.json({ error: error.response.data.error })
-    return res.json({ error: util.inspect(error) })
+    return res.json({ error: util.inspect(err) })
   }
 }
 
 // Retrieve UTXO information for an address.
-async function utxo(req, res, next) {
+async function utxo(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
   try {
     const addresses = req.body.addresses
 
@@ -178,19 +206,27 @@ async function utxo(req, res, next) {
     res.status(200)
     return res.json(retArray)
   } catch (err) {
-    // Write out error to error log.
-    //logger.error(`Error in address/details: `, error)
+    // Attempt to decode the error message.
+    const { msg, status } = routeUtils.decodeError(err)
+    if (msg) {
+      res.status(status)
+      return res.json({ error: msg })
+    }
 
-    // Return error message to the caller.
+    // Write out error to error log.
+    //logger.error(`Error in rawtransactions/decodeRawTransaction: `, err)
+
     res.status(500)
-    if (error.response && error.response.data && error.response.data.error)
-      return res.json({ error: error.response.data.error })
-    return res.json({ error: util.inspect(error) })
+    return res.json({ error: util.inspect(err) })
   }
 }
 
 // Retrieve any unconfirmed TX information for a given address.
-async function unconfirmed(req, res, next) {
+async function unconfirmed(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
   try {
     const addresses = req.body.addresses
 
@@ -249,19 +285,27 @@ async function unconfirmed(req, res, next) {
     res.status(200)
     return res.json(retArray)
   } catch (err) {
-    // Write out error to error log.
-    //logger.error(`Error in address/details: `, error)
+    // Attempt to decode the error message.
+    const { msg, status } = routeUtils.decodeError(err)
+    if (msg) {
+      res.status(status)
+      return res.json({ error: msg })
+    }
 
-    // Return error message to the caller.
+    // Write out error to error log.
+    //logger.error(`Error in rawtransactions/decodeRawTransaction: `, err)
+
     res.status(500)
-    if (error.response && error.response.data && error.response.data.error)
-      return res.json({ error: error.response.data.error })
-    return res.json({ error: util.inspect(error) })
+    return res.json({ error: util.inspect(err) })
   }
 }
 
 // Get an array of TX information for a given address.
-async function transactions(req, res, next) {
+async function transactions(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
   try {
     const addresses = req.body.addresses
 
@@ -316,14 +360,18 @@ async function transactions(req, res, next) {
     res.status(200)
     return res.json(retArray)
   } catch (err) {
-    // Write out error to error log.
-    //logger.error(`Error in address/details: `, error)
+    // Attempt to decode the error message.
+    const { msg, status } = routeUtils.decodeError(err)
+    if (msg) {
+      res.status(status)
+      return res.json({ error: msg })
+    }
 
-    // Return error message to the caller.
+    // Write out error to error log.
+    //logger.error(`Error in rawtransactions/decodeRawTransaction: `, err)
+
     res.status(500)
-    if (error.response && error.response.data && error.response.data.error)
-      return res.json({ error: error.response.data.error })
-    return res.json({ error: util.inspect(error) })
+    return res.json({ error: util.inspect(err) })
   }
 }
 
