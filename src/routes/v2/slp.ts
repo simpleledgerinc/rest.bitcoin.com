b95700731e6772b7a32e9b70795e66f8c0e9ef6d
Gabriel Cardona <gabriel@bitcoin.com>
2019-01-28 11:49:29 +0900

    Swap URLs to test.

--- src/routes/v2/slp.ts
+++ src/routes/v2/slp.ts
@@ -449,9 +449,9 @@ async function validateBulk(
 async function isValidSlpTxid(txid: string) {
   let result
   try {
-    result = await validateTx(txid, process.env.SLP_VALIDATE_URL)
-  } catch (err) {
     result = await validateTx(txid, process.env.SLP_VALIDATE_FAILOVER_URL)
+  } catch (err) {
+    result = await validateTx(txid, process.env.SLP_VALIDATE_URL)
   }
 
   if (result === true) {
