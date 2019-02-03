6b0d28b367d0c99597cc12b52ff259e532d6b57f
Gabriel Cardona <gabriel@bitcoin.com>
2019-02-04 07:33:43 +0900

    Add v2 route.

--- src/routes/v2/index.js
+++ src/routes/v2/index.js
@@ -19,8 +19,27 @@ const indexRateLimit1 = new RateLimit({
   }
 })
 
+const indexRateLimit2 = new RateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour window
+  delayMs: 0, // disable delaying - full speed until the max limit is reached
+  max: 60, // start blocking after 60 requests
+  handler: function(req, res /*next*/) {
+    res.format({
+      json: function() {
+        res.status(500).json({
+          error: "Too many requests. Limits are 60 requests per minute."
+        })
+      }
+    })
+  }
+})
+
 /* GET home page. */
 router.get("/", indexRateLimit1, (req, res, next) => {
+  res.render("swagger-v2")
+})
+
+router.get("/v2", indexRateLimit2, (req, res, next) => {
   res.render("swagger-v2")
 })
 
