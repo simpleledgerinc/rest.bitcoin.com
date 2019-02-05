f0b62671d583626977cf9066dcf23f3ec087a7e6
SpendBCH <SpendBCH@SpendBCH.io>
2019-02-05 15:25:01 +0900

    delete wormhole socket endpoint

--- dist/app.js
+++ dist/app.js
@@ -108,22 +108,6 @@ app.use("/" + v2prefix + "/" + "dataRetrieval", dataRetrievalV2.router);
 app.use("/" + v2prefix + "/" + "payloadCreation", payloadCreationV2.router);
 app.use("/" + v2prefix + "/" + "wormhole/transaction", wormholeV2.router);
 app.use("/" + v2prefix + "/" + "slp", slpV2.router);
-// Initialize wormhole/transaction/socket endpoint
-var whSocketUrl = process.env.WORMHOLE_SOCKET_URL;
-var whSocketPort = process.env.WORMHOLE_SOCKET_PORT;
-if (whSocketUrl && whSocketPort) {
-    var bchsocketd = require("bchsocketd");
-    bchsocketd.init({
-        bit: {
-            host: whSocketUrl,
-            port: whSocketPort
-        },
-        socket: {
-            app: app,
-            endpoint: v2prefix + "/wormhole/transaction/socket"
-        }
-    });
-}
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = {
--- src/app.ts
+++ src/app.ts
@@ -151,23 +151,6 @@ app.use(`/${v2prefix}/` + `payloadCreation`, payloadCreationV2.router)
 app.use(`/${v2prefix}/` + `wormhole/transaction`, wormholeV2.router)
 app.use(`/${v2prefix}/` + `slp`, slpV2.router)
 
-// Initialize wormhole/transaction/socket endpoint
-const whSocketUrl = process.env.WORMHOLE_SOCKET_URL
-const whSocketPort = process.env.WORMHOLE_SOCKET_PORT
-if (whSocketUrl && whSocketPort) {
-  const bchsocketd = require("bchsocketd")
-  bchsocketd.init({
-    bit: {
-      host: whSocketUrl,
-      port: whSocketPort
-    },
-    socket: {
-      app: app,
-      endpoint: `${v2prefix}/wormhole/transaction/socket`
-    }
-  })
-}
-
 // catch 404 and forward to error handler
 app.use(
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
