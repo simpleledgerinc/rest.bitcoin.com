121dc6ffad77df0fa317bd6493f69893eaa0c2f2
Gabriel Cardona <gabriel@bitcoin.com>
2019-02-06 08:22:20 +0900

    Remove wormhole socket and up timeout.

--- dist/app.js
+++ dist/app.js
@@ -108,22 +108,6 @@ app.use("/" + v2prefix + "/" + "dataRetrieval", dataRetrievalV2.router);
 app.use("/" + v2prefix + "/" + "payloadCreation", payloadCreationV2.router);
 app.use("/" + v2prefix + "/" + "wormhole/transaction", wormholeV2.router);
 app.use("/" + v2prefix + "/" + "slp", slpV2.router);
-// Initialize wormhole/transaction/socket endpoint
-var whSocketUrl = process.env.WORMHOLE_SOCKET_URL;
-var whSocketPort = process.env.WORMHOLE_SOCKET_PORT;
-if (whSocketUrl && whSocketPort) {
-    var bchsocketd = require("bchsocketd");
-    bchsocketd.init({
-        bit: {
-            host: whSocketUrl,
-            port: whSocketPort
-        },
-        socket: {
-            app: app,
-            endpoint: v2prefix + "/wormhole/transaction/socket"
-        }
-    });
-}
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = {
@@ -184,7 +168,7 @@ server.on("error", onError);
 server.on("listening", onListening);
 // Set the time before a timeout error is generated. This impacts testing and
 // the handling of timeout errors. Is 10 seconds too agressive?
-server.setTimeout(10000);
+server.setTimeout(30 * 1000);
 /**
  * Normalize a port into a number, string, or false.
  */
