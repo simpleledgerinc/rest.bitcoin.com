a4cc3e78efa3a0a8cfff5f53d6ba83c8e192d597
Chris Troutner <chris.troutner@gmail.com>
2019-02-06 10:10:36 -0800

    Fixed unit test for slp/list

--- test/v2/mocks/slp-mocks.js
+++ test/v2/mocks/slp-mocks.js
@@ -8,32 +8,279 @@ const mockList = {
   u: [],
   c: [
     {
-      id: "650dea14c77f4d749608e36e375450c9ac91deb8b1b53e50cb0de2059a52d19a",
-      timestamp: "2018-08-26 07:06",
-      symbol: "",
-      name: "TESTYCOIN",
-      document: ""
+      _id: "5bf4ac84362c330ef0a13251",
+      tx: {
+        h: "b9ebf074f4d5ee884d5656107b0d70077089814b61dbf40ccaade4660451841a"
+      },
+      in: [
+        {
+          i: 0,
+          b0:
+            "MEUCIQDGsKyS/oXUYVyDmmRwDto6BOIEOCwtuR8N82FvKDVgjAIge+JVnrFw7d49rJiGlPoBV1D8EPWJgKLcW+ligYBflNdB",
+          b1: "A43YD78Is4JDRq+ZBVihUrvo719/oSrn9ecLEwcah1Pk",
+          str:
+            "3045022100c6b0ac92fe85d4615c839a64700eda3a04e204382c2db91f0df3616f2835608c02207be2559eb170edde3dac988694fa015750fc10f58980a2dc5be96281805f94d741 038dd80fbf08b3824346af990558a152bbe8ef5f7fa12ae7f5e70b13071a8753e4",
+          e: {
+            h:
+              "c798b8bc8346cec59e2dd489309476790a39bbd9c7714810b63b97cbb26478dc",
+            i: 2,
+            a: "qrmuav9q86t5424dfzdezsyxq54xe43fvuv6n89spm"
+          },
+          h0:
+            "3045022100c6b0ac92fe85d4615c839a64700eda3a04e204382c2db91f0df3616f2835608c02207be2559eb170edde3dac988694fa015750fc10f58980a2dc5be96281805f94d741",
+          h1:
+            "038dd80fbf08b3824346af990558a152bbe8ef5f7fa12ae7f5e70b13071a8753e4"
+        }
+      ],
+      out: [
+        {
+          i: 0,
+          b0: {
+            op: 106
+          },
+          b1: "U0xQAA==",
+          s1: "SLP\u0000",
+          b2: "AQ==",
+          s2: "\u0001",
+          b3: "R0VORVNJUw==",
+          s3: "GENESIS",
+          b4: "UFNO",
+          s4: "PSN",
+          b5: "UG9zdGVybnV0",
+          s5: "Posternut",
+          b6: "",
+          s6: "",
+          b7: "",
+          s7: "",
+          b8: "CA==",
+          s8: "\b",
+          b9: "",
+          s9: "",
+          b10: "AAAABKgXyAA=",
+          s10: "\u0000\u0000\u0000\u0004�\u0017�\u0000",
+          str:
+            "OP_RETURN 534c5000 01 47454e45534953 50534e 506f737465726e7574 OP_PUSHDATA1 OP_PUSHDATA1 08 OP_PUSHDATA1 00000004a817c800",
+          e: {
+            v: 0,
+            i: 0
+          },
+          h1: "534c5000",
+          h2: "01",
+          h3: "47454e45534953",
+          h4: "50534e",
+          h5: "506f737465726e7574",
+          h6: "",
+          h7: "",
+          h8: "08",
+          h9: "",
+          h10: "00000004a817c800"
+        },
+        {
+          i: 1,
+          b0: {
+            op: 118
+          },
+          b1: {
+            op: 169
+          },
+          b2: "xYP115l2UUVWZGdK9bGNSzfFbi8=",
+          s2: "Ń�יvQEVdgJ���K7�n/",
+          b3: {
+            op: 136
+          },
+          b4: {
+            op: 172
+          },
+          str:
+            "OP_DUP OP_HASH160 c583f5d7997651455664674af5b18d4b37c56e2f OP_EQUALVERIFY OP_CHECKSIG",
+          e: {
+            v: 546,
+            i: 1,
+            a: "qrzc8awhn9m9z32kv3n54ad3349n03tw9ufm74p0gp"
+          },
+          h2: "c583f5d7997651455664674af5b18d4b37c56e2f"
+        },
+        {
+          i: 2,
+          b0: {
+            op: 118
+          },
+          b1: {
+            op: 169
+          },
+          b2: "R7gF3+DV6tzBk59HC4YZhgDZ9r0=",
+          s2: "G�\u0005��������G\u000b�\u0019�\u0000���",
+          b3: {
+            op: 136
+          },
+          b4: {
+            op: 172
+          },
+          str:
+            "OP_DUP OP_HASH160 47b805dfe0d5eadcc1939f470b86198600d9f6bd OP_EQUALVERIFY OP_CHECKSIG",
+          e: {
+            v: 291998,
+            i: 2,
+            a: "qprmspwlur274hxpjw05wzuxrxrqpk0kh54n2dets8"
+          },
+          h2: "47b805dfe0d5eadcc1939f470b86198600d9f6bd"
+        }
+      ],
+      blk: {
+        i: 544611,
+        h: "000000000000000000490d30bc5ee54866ed0b3057afa30b4e621997a1ca742d",
+        t: 1534989459
+      }
     },
     {
-      id: "74d5c1b2916273f19d1a8e80536658f2d63fb337353d9cf108af62d8c6a65154",
-      timestamp: "2018-12-08 17:04",
-      symbol: "J9T",
-      name: "J9-T",
-      document: ""
-    },
-    {
-      id: "8095d290d215975bc87bb34fd91b73f2cad60ba4949bb33791c1224e3b594a6f",
-      timestamp: "2018-12-24 13:03",
-      symbol: "B13",
-      name: "B13",
-      document: ""
-    },
-    {
-      id: "730741cb0aa0d05110fdcc079a5e1f5a4acc86bd504bd00e26ddb514d1242f76",
-      timestamp: "2018-12-24 16:57",
-      symbol: "",
-      name: "",
-      document: ""
+      _id: "5bf4ac81362c330ef0a1306e",
+      tx: {
+        h: "150ae494f1e0ddde8675b83c6e7dae73e51eb7578f4dd9a7b0716a61915abad4"
+      },
+      in: [
+        {
+          i: 0,
+          b0:
+            "MEUCIQCRVpW1N0jZKFWI2sLVAwU0DrbQvVz/BARFeetJC5BtngIgL7JULI5o7qYUDMtsWHQdz0KiOCf9CXmt6eqAMyENnxxB",
+          b1: "AlcXBlqktUmh5eBYYn6NfanYtzRcKyv7STQpgvJOqn+2",
+          str:
+            "3045022100915695b53748d9285588dac2d50305340eb6d0bd5cff04044579eb490b906d9e02202fb2542c8e68eea6140ccb6c58741dcf42a23827fd0979ade9ea8033210d9f1c41 025717065aa4b549a1e5e058627e8d7da9d8b7345c2b2bfb49342982f24eaa7fb6",
+          e: {
+            h:
+              "c5f4970f3ca276f5a690202e9e5cf14fc49d604029f45c7ab9841a439eaba8bd",
+            i: 0,
+            a: "qr884rkh6833ackmr6dt7ukxtwpyg0qa3q8dh0s3pm"
+          },
+          h0:
+            "3045022100915695b53748d9285588dac2d50305340eb6d0bd5cff04044579eb490b906d9e02202fb2542c8e68eea6140ccb6c58741dcf42a23827fd0979ade9ea8033210d9f1c41",
+          h1:
+            "025717065aa4b549a1e5e058627e8d7da9d8b7345c2b2bfb49342982f24eaa7fb6"
+        }
+      ],
+      out: [
+        {
+          i: 0,
+          b0: {
+            op: 106
+          },
+          b1: "U0xQAA==",
+          s1: "SLP\u0000",
+          b2: "AQ==",
+          s2: "\u0001",
+          b3: "R0VORVNJUw==",
+          s3: "GENESIS",
+          b4: "VFJU",
+          s4: "TRT",
+          b5: "VGFydGFydXNUb2tlbg==",
+          s5: "TartarusToken",
+          b6: "aHR0cHM6Ly93d3cudGFydGFydXNncm91cC5jb20=",
+          s6: "https://www.tartarusgroup.com",
+          b7: "",
+          s7: "",
+          b8: "Ag==",
+          s8: "\u0002",
+          b9: "Ag==",
+          s9: "\u0002",
+          b10: "AAAAAABhqAA=",
+          s10: "\u0000\u0000\u0000\u0000\u0000a�\u0000",
+          str:
+            "OP_RETURN 534c5000 01 47454e45534953 545254 5461727461727573546f6b656e 68747470733a2f2f7777772e746172746172757367726f75702e636f6d OP_PUSHDATA1 02 02 000000000061a800",
+          e: {
+            v: 0,
+            i: 0
+          },
+          h1: "534c5000",
+          h2: "01",
+          h3: "47454e45534953",
+          h4: "545254",
+          h5: "5461727461727573546f6b656e",
+          h6: "68747470733a2f2f7777772e746172746172757367726f75702e636f6d",
+          h7: "",
+          h8: "02",
+          h9: "02",
+          h10: "000000000061a800"
+        },
+        {
+          i: 1,
+          b0: {
+            op: 118
+          },
+          b1: {
+            op: 169
+          },
+          b2: "znqO19HjHuLbHpq/csZbgkQ8HYg=",
+          s2: "�z����\u001e��\u001e��r�[�D<\u001d�",
+          b3: {
+            op: 136
+          },
+          b4: {
+            op: 172
+          },
+          str:
+            "OP_DUP OP_HASH160 ce7a8ed7d1e31ee2db1e9abf72c65b82443c1d88 OP_EQUALVERIFY OP_CHECKSIG",
+          e: {
+            v: 546,
+            i: 1,
+            a: "qr884rkh6833ackmr6dt7ukxtwpyg0qa3q8dh0s3pm"
+          },
+          h2: "ce7a8ed7d1e31ee2db1e9abf72c65b82443c1d88"
+        },
+        {
+          i: 2,
+          b0: {
+            op: 118
+          },
+          b1: {
+            op: 169
+          },
+          b2: "znqO19HjHuLbHpq/csZbgkQ8HYg=",
+          s2: "�z����\u001e��\u001e��r�[�D<\u001d�",
+          b3: {
+            op: 136
+          },
+          b4: {
+            op: 172
+          },
+          str:
+            "OP_DUP OP_HASH160 ce7a8ed7d1e31ee2db1e9abf72c65b82443c1d88 OP_EQUALVERIFY OP_CHECKSIG",
+          e: {
+            v: 546,
+            i: 2,
+            a: "qr884rkh6833ackmr6dt7ukxtwpyg0qa3q8dh0s3pm"
+          },
+          h2: "ce7a8ed7d1e31ee2db1e9abf72c65b82443c1d88"
+        },
+        {
+          i: 3,
+          b0: {
+            op: 118
+          },
+          b1: {
+            op: 169
+          },
+          b2: "g75airTfvw67+N5FyVGhmGysKeY=",
+          s2: "��Z��߿\u000e���E�Q��l�)�",
+          b3: {
+            op: 136
+          },
+          b4: {
+            op: 172
+          },
+          str:
+            "OP_DUP OP_HASH160 83be5a8ab4dfbf0ebbf8de45c951a1986cac29e6 OP_EQUALVERIFY OP_CHECKSIG",
+          e: {
+            v: 190524,
+            i: 3,
+            a: "qzpmuk52kn0m7r4mlr0ytj235xvxetpfucr9g0jm4u"
+          },
+          h2: "83be5a8ab4dfbf0ebbf8de45c951a1986cac29e6"
+        }
+      ],
+      blk: {
+        i: 544603,
+        h: "0000000000000000019e5d777a2daf58dcf1b24095ab891e3dc19bf1a21bda04",
+        t: 1534986534
+      }
     }
   ]
 }
